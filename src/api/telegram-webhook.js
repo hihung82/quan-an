e63a337421667cd2389225e4f6ff2d307093cd41
24022333 <24022333@vnu.edu.vn>
2026-03-07 17:30:36 +0700

    update

--- src/api/telegram-webhook.js
+++ src/api/telegram-webhook.js
@@ -1,33 +1,11 @@
-import { supabase } from "../src/services/supabase"
-
 export default async function handler(req, res) {
 
+  // chỉ chấp nhận POST
   if (req.method !== "POST") {
     return res.status(200).send("ok")
   }
 
   console.log("TELEGRAM DATA:", req.body)
 
-  const message = req.body?.message
-  if (!message) return res.status(200).send("ok")
-
-  const chatId = message.chat.id
-  const text = message.text || ""
-
-  if (text.startsWith("/start")) {
-
-    const shopId = text.split(" ")[1]
-
-    if (shopId) {
-      await supabase
-        .from("telegram_connections")
-        .insert({
-          shop_id: shopId,
-          chat_id: chatId
-        })
-    }
-
-  }
-/* note*/
   res.status(200).send("ok")
 }
