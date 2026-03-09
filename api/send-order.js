4328a5a2831cbf5c30b7cf2768f5499ae68c138a
24022333 <24022333@vnu.edu.vn>
2026-03-09 13:21:09 +0700

    update

--- api/send-order.js
+++ api/send-order.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
     .from("telegram_connections")
     .select("chat_id")
     .eq("shop_id", shop_id)
-    .single()
+    .maybeSingle()
 
   if (!data) {
     return res.status(400).json({ error: "Shop chưa connect telegram" })
